bcefba96baaa5bc3bd0d99465d3dda55d49bbe89
Fletcher Moore <fletcherjmoore14@gmail.com>
2024-08-10 11:51:49 -0500

    commiting changes to tags

---
+++ api/tagData.js
@@ -0,0 +1,34 @@
+import clientCredentials from '../utils/client';
+
+const endpoint = clientCredentials.databaseURL;
+
+const getAllTags = () => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/tags`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const createTag = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/tags`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then(resolve)
+    .catch(reject);
+});
+
+export
+{
+  getAllTags,
+  createTag,
+};
