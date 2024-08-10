43f10acd3560d0d336851ee63af4e67f1ac8a861
Britnay Gore <britnaygore268@gmail.com>
2024-08-10 13:29:19 -0400

    Added route to add a category

--- components/CategoriesCard.js
+++ components/CategoriesCard.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { ListGroup } from 'react-bootstrap';
 import PropTypes from 'prop-types';
-// import { getAllCategories } from '../api/categoriesData';
-// import { Card, CardText } from 'react-bootstrap';
 
 export default function CategoryCard({ categoryObj }) {
   return (
