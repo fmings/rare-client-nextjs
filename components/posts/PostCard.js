e5b3cba76744d939db0efd46bae7da12f5b0cc2a
fmings <mingsfelicia@gmail.com>
2024-08-11 08:59:00 -0500

    added files and functionality to view post details

--- components/posts/PostCard.js
+++ components/posts/PostCard.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
 import PropTypes from 'prop-types';
+import Link from 'next/link';
 
 export default function PostCard({ postObj }) {
-  // INSERT FUNCTION TO GET USERS/AUTHORS and CATEGORIES ONCE PROMISES ARE BUILT
   return (
     <div>
+      {console.warn(postObj)}
       <Card style={{ width: '18rem' }}>
         <Card.Body>
-          <Card.Title>{postObj.title}</Card.Title>
+          <Link passHref href={`/post/${postObj.id}`}>
+            <Card.Title>{postObj.title}</Card.Title>
+          </Link>
           <Card.Subtitle className="mb-2 text-muted">{postObj.publicationDate}</Card.Subtitle>
-          <Card.Subtitle className="mb-2 text-muted">Category: {postObj.categoryId}</Card.Subtitle>
+          <Card.Subtitle className="mb-2 text-muted">Category: {postObj.category.label}</Card.Subtitle>
           <Card.Img className="post-image" variant="top" src={postObj.imageUrl} />
           <Card.Text>
             {postObj.content}
           </Card.Text>
-          <Card.Subtitle className="mb-2 text-muted">Author: {postObj.userId}</Card.Subtitle>
-          <Card.Link href="#">View Post</Card.Link>
+          <Card.Subtitle className="mb-2 text-muted">Author: {postObj.user.firstName} {postObj.user.lastName}</Card.Subtitle>
         </Card.Body>
       </Card>
     </div>
@@ -33,5 +35,12 @@ PostCard.propTypes = {
     imageUrl: PropTypes.string,
     content: PropTypes.string,
     approved: PropTypes.bool,
+    category: PropTypes.shape({
+      label: PropTypes.string,
+    }),
+    user: PropTypes.shape({
+      firstName: PropTypes.string,
+      lastName: PropTypes.string,
+    }),
   }).isRequired,
 };
